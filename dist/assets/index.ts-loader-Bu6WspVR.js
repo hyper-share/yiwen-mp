662b5e575e301db563ce7d2f37363fa1789c7d20
huanghaipeng01 <huanghaipeng01@58.com>
2026-06-12 14:22:28 +0800

    build: update dist and zip package

--- dist/assets/index.ts-loader-Bu6WspVR.js
+++ dist/assets/index.ts-loader-Bu6WspVR.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.ts-DOuHiR6Q.js")
+      chrome.runtime.getURL("assets/index.ts-yGZdVcG3.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
